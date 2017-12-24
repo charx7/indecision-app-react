701cce9609d1cb6180663c4a51c940b6aeedc19a
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-23 23:07:35 -0600

    Importaciones de react y reactdom usando webpack y yarn

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('app.js corre xd mafia');
+// Importaciones de React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'ola k ase');
+ReactDOM.render(template, document.getElementById('app'));
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+console.log('utils.js corre');
+
+const square = (x) => x * x;
+
+const add = (a,b) => a + b;
+
+const substract = (a,b) => a - b;
+
+export {square, add, substract as default };
+// exportaciones con nombre
+//export {square, add};
+
