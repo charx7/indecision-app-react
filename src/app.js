5230dcbcb9ec5ba98eca2c5aa6c07136b9d0b495
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-24 17:45:32 -0600

    Establecida la configuracion de Webpack para trabajar con estilos de CSS

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.css';
 
 // Rendereo de toda la aplicacion usando el componente padre IndecisionApp
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,19 @@ module.exports = {
     // Configuracion del loader
     module: {
         rules: [{
+            //Establecemos los loaders de BABEL
             loader: 'babel-loader',
             // verifica si el archivo termina en .js para correr el babel-loader
             test: /\.js$/,
             exclude: /node_modules/
+        }, {
+            //Reglas para seleccionar todos los archivos que terminan en .css y se tranformen
+            test: /\.css$/,
+            // Establecemos los loaders de CSS
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
         }]
     },
     // Devtools para debugear sin pasar por el codigo del bundle.js
