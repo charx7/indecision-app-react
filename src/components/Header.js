d8fdcf315f3c7ca97775a303eb9706b10806651f
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-24 18:08:03 -0600

    Establecidos estilos basicos de SCSS usando parciales

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = (props) =>{
     // Rendereo del HTML (JSX) en este tipo de componentes se obtiene acceso a los props de manera directa 
     // sin hacer referencia a this.props => props
     return (
-        <div>
+        <div className= "header">
             {/* Usando props para mandar una variable de JS al rendereo del componente*/}
             <h1>{props.titulo}</h1>
             {/* Renderio del subtitulo solo si existe*/}
